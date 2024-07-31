bb0b808ff6ef5a510a6d932987f6f4627dadbdf7
guanwei <guanw01@mingyuanyun.com>
2024-07-31 09:55:36 +0800

    开始实现cli形式

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,32 @@
-import path from 'path';
-
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve'; // 引用第三方包
 import typescript from 'rollup-plugin-typescript2';
 // import babel from 'rollup-plugin-babel';
+import preserveShebang from 'rollup-plugin-preserve-shebang';
 
-import pkg from './package.json';
+const inputArr = [
+  {
+    input: 'src/index.ts',
+    main: 'dist/index.cjs.js',
+    module: 'dist/index.esm.js',
+  },
+  {
+    input: 'src/cli/index.ts',
+    main: 'dist/cli.cjs.js',
+    module: 'dist/cli.esm.js',
+  },
+];
 
-const resolve = (...args) => path.resolve(__dirname, ...args);
-
-export default {
-  input: 'src/index.ts',
+const rollupConfig = inputArr.map(({ input, main, module }) => ({
+  input,
   output: [
     {
-      file: resolve(pkg.main),
+      file: main,
       format: 'cjs',
       exports: 'named',
     },
     {
-      file: resolve(pkg.module),
+      file: module,
       format: 'es',
       exports: 'named',
     },
@@ -26,6 +34,7 @@ export default {
   plugins: [
     commonjs(),
     nodeResolve(),
+    preserveShebang(),
     typescript({
       tsconfig: './tsconfig.json',
       clean: true,
@@ -41,4 +50,6 @@ export default {
     include: 'src/**', // 监听 src 目录下的所有文件
     exclude: 'node_modules/**', // 排除 node_modules 目录
   },
-};
+}));
+
+export default rollupConfig;
---
+++ src/cli/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+// import commander from 'commander';
+
+import CodeAnalysis from '../analysis';
+import { writeFile } from '../utils/file';
+
+// const program = commander.Command;
